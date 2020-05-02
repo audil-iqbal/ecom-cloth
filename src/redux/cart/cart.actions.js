d9f23f3ba5659ab40df90702d342fa780fbdac2d
audil.iqbal <audil.iqbal000@gmail.com>
2020-05-03 01:55:40 +0530

    additem and removeitem

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,6 +1,12 @@
 export const TOGGLE_CART_HIDDEN = 'TOGGLE_CART_HIDDEN';
 export const ADD_ITEM = 'ADD_ITEM';
+export const CLEAR_ITEM_FROM_CART = 'CLEAR_ITEM_FROM_CART';
+export const REMOVE_ITEM = 'REMOVE_ITEM';
 
+export const removeItem = item => ({
+    type: REMOVE_ITEM,
+    payload: item
+})
 
 export const toggleCartHidden = () => ({
     type: TOGGLE_CART_HIDDEN
@@ -11,3 +17,7 @@ export const addItem = item => ({
     payload: item,
 });
  
+export const clearItemFromCart = item => ({
+    type: CLEAR_ITEM_FROM_CART,
+    payload: item
+})
