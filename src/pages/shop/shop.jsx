c5ba418b355704f50632c83459fa16a0bca7f04d
audil.iqbal <audil.iqbal000@gmail.com>
2020-05-18 00:23:29 +0530

    moved data into reducer

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,27 +1,15 @@
-import React, { Component } from "react";
-import SHOP_DATA from "./shopdata";
-import CollectionPreview from "../../components/collection-preview/preview";
+import React from "react";
+import { Route } from "react-router-dom";
 
-class ShopPage extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+import CollectionPage from "../collection/collection";
+import CollectionOverview from "../../components/collections-overview/collections-overview";
 
-    render(){
-        const {collections} = this.state;
-        return(
-            <div className='shop-page'>
-                {
-                    collections.map(({id, ...collectionProps}) => (
-                        <CollectionPreview key={id} {...collectionProps} />
-                    ))
-                }
-            </div>
-        );
-    }
-}
+const ShopPage = ({match}) => (
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+);
+
 
 export default ShopPage;
